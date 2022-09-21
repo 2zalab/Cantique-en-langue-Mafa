cb947f76baa0c206e530eb575746a19b9c09bd89
Touza Isaac <62522441+Touza-Isaac@users.noreply.github.com>
2022-09-21 23:35:01 +0100

    Cantique en langue Mafa
    Recueil des chants en langue Mafa (langue locale au nord du Cameroun)

---
+++ app/src/main/java/com/sintel/cantiqueenmafa/MainActivity.java
@@ -0,0 +1,34 @@
+package com.sintel.cantiqueenmafa;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.sintel.cantiqueenmafa.ui.main.SectionsPagerAdapter;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+}
